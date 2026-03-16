74cf71c8c19badbaaa8df21c987743a89cf24ace
OleksiiChernov-general <chernov.alex.2611@gmail.com>
2026-03-16 22:20:13 +0300

    Update __main__.py

--- site_document_unloader/__main__.py
+++ site_document_unloader/__main__.py
@@ -1,4 +1,17 @@
-from .cli import main
+from pathlib import Path
 
-if __name__ == "__main__":
-    main()
+project_root = Path(__file__).resolve().parent
+datas = []
+
+config_yaml = project_root / "config.yaml"
+config_example = project_root / "config.example.yaml"
+start_bat = project_root / "start.bat"
+
+if config_yaml.exists():
+    datas.append((str(config_yaml), "."))
+
+if config_example.exists():
+    datas.append((str(config_example), "."))
+
+if start_bat.exists():
+    datas.append((str(start_bat), "."))
